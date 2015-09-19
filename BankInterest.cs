a46709cbd8f4a30c6d87350b25839ec94f115835
Timotei Dig <timoteidig@yahoo.com>
2015-09-19 10:04:31 +0300

    test 8

--- BankInterest.cs
+++ BankInterest.cs
@@ -5,13 +5,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PrimitiveTypes
 {
-    /// <summary>
-    /// Summary description for UnitTest1
-    /// </summary>
+    // vrei să iei un credit de 40k euro de la bancă
+    // dobânda anuală cerută de bancă e de 7.57%
+    // creditul e pentru o perioadă de 20 ani
+    // contractul prevede restituirea în rate descrescătoare
+
+    // calculează ce rată trebuie plătită în luna martie din al celui de al 4-lea an?
+    // generalizează pentru orice sumă, perioadă și dobândă<summary>
+    
     [TestClass]
     public class BankInterest
     {
         [TestMethod]
+        //Test with initial values
         public void Test1()
         {
             double creditTotalAmmount = 40000;
@@ -25,7 +31,7 @@ namespace PrimitiveTypes
         }
 
         [TestMethod]
-        //Credit given is 0 or less than 0
+        //Credit ammount is 0 or less than 0
         public void Test2()
         {
             double creditTotalAmmount = 0;
@@ -95,7 +101,7 @@ namespace PrimitiveTypes
         }
 
         [TestMethod]
-        //The monthly payment have to be only in the credit period
+        //The monthly payment have to be only in the credit period.  
         public void Test7()
         {
             double creditTotalAmmount = 40000;
@@ -108,11 +114,25 @@ namespace PrimitiveTypes
             Assert.AreEqual(bankRateMonthX_Expected, bankRateMonthX_Actual);
         }
 
+        [TestMethod]
+        //The monthly payment have to start with month 1
+        public void Test8()
+        {
+            double creditTotalAmmount = 40000;
+            int creditPeriodYears = 20;
+            double creditAnualInterest = 7.57;
+            int monthNumberX = 0;
+            double bankRateMonthX_Expected = 0;
+            double bankRateMonthX_Actual = BankRate(creditTotalAmmount, creditPeriodYears, creditAnualInterest, monthNumberX);
+
+            Assert.AreEqual(bankRateMonthX_Expected, bankRateMonthX_Actual);
+        }
+
 
         private double BankRate(double creditTotalAmmount, int creditPeriodYears, double creditAnualInterest, int monthNumberX)
         {
             int creditPeriodMonths = creditPeriodYears * 12;
-
+            //Check if the Credit makes any sense. 
             if(creditTotalAmmount <= 0 || creditPeriodYears <= 0 || creditPeriodYears > 35 || creditAnualInterest <= 0 || creditAnualInterest > 100 || monthNumberX <= 0 || monthNumberX > creditPeriodMonths)
             {
                 return 0;
@@ -122,11 +142,9 @@ namespace PrimitiveTypes
                 int monthsAlreadyPayed = monthNumberX - 1;
                 double creditSoldInMonthX = creditTotalAmmount - creditMonthlyPaymentWithoutInterest * monthsAlreadyPayed;
 
-                //Interest for a particular mounth is the remaining sold * anual interest. 
-                //Have to be devided with 100 because the interest is %. 
-                //Also have to be devided with 12 because is for one month
+                //Interest for a particular month is the remaining sold * anual interest / divided by 12.
                 double creditInterestInMonthX = creditSoldInMonthX * creditAnualInterest / 100 / 12;
-                //We have to add the monthly payment from sold
+                //We have to add the monthly payment from sold to the computed monthly interest
                 double bankRate = creditMonthlyPaymentWithoutInterest + creditInterestInMonthX;
 
                 return Math.Round(bankRate, 2);
