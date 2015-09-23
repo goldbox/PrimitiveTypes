ab51e6ab06bd2a492581d37492d586cfdf9b5928
timoteid <timoteidig@yahoo.com>
2015-09-23 14:49:04 +0300

    Bird and Trains

---
+++ BirdAndTrains.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimitiveTypes
+{
+    [TestClass]
+    public class BirdAndTrains
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            int trainSpeed = 10;
+            double initialDistanceBetweenTrains = 1500;
+            double birdDistanceExpected = 750;
+            double birdDistanceActual = CalculateBirdDistance(trainSpeed, initialDistanceBetweenTrains);
+            Assert.AreEqual(birdDistanceExpected, birdDistanceActual);
+        }
+
+        [TestMethod]
+        //Trains are stopped
+        public void Test2()
+        {
+            int trainSpeed = 0;
+            double initialDistanceBetweenTrains = 1500;
+            double birdDistanceExpected = 0;
+            double birdDistanceActual = CalculateBirdDistance(trainSpeed, initialDistanceBetweenTrains);
+            Assert.AreEqual(birdDistanceExpected, birdDistanceActual);
+        }
+
+        [TestMethod]
+        //initial distance between trains is 0
+        public void Test3()
+        {
+            int trainSpeed = 10;
+            double initialDistanceBetweenTrains = 0;
+            double birdDistanceExpected = 0;
+            double birdDistanceActual = CalculateBirdDistance(trainSpeed, initialDistanceBetweenTrains);
+            Assert.AreEqual(birdDistanceExpected, birdDistanceActual);
+        }
+
+        private double CalculateBirdDistance (int trainSpeed, double initialDistanceBetweenTrains)
+        {
+            if (trainSpeed <= 0 || initialDistanceBetweenTrains <= 0)
+            {
+                return 0;
+            } else
+            {
+                // Bird flies 2 times faster than train speed. 
+                // Since we have 2 trains the distance bird flies = distance made by both trains combined. 
+                // bird start flying only after both trains make half of initial distance, 1/4 each train. 
+                return initialDistanceBetweenTrains / 2;
+            }
+        }
+    }
+}
