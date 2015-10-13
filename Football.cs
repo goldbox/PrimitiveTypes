dfd0a8d9beb66a4e56862a8739c777d686c20c79
timoteid <timoteidig@yahoo.com>
2015-10-13 14:41:03 +0300

    Football Improved

--- Football.cs
+++ Football.cs
@@ -7,46 +7,18 @@ namespace PrimitiveTypes
 {
     public struct FootballGame
     {
-        private string _team1;
-        private string _team2;
-        private int _scoreTeam1;
-        private int _scoreTeam2;
-
-
-        public string Team1
-        {
-            get { return this._team1; }
-            set { this._team1 = value; }
-        }
-
-        public string Team2
-        {
-            get { return this._team2; }
-            set { this._team2 = value; }
-        }
-
-        public int ScoreTeam1
-        {
-            get { return this._scoreTeam1; }
-            set { this._scoreTeam1 = value; }
-
-        }
-
-        public int ScoreTeam2
-        {
-            get { return this._scoreTeam2; }
-            set { this._scoreTeam2 = value; }
-        }
+        public string team1;
+        public string team2;
+        public int scoreTeam1;
+        public int scoreTeam2;
 
         public FootballGame(string team1, string team2, int scoreTeam1, int scoreTeam2)
         {
-            this._team1 = team1;
-            this._scoreTeam1 = scoreTeam1;
-            this._team2 = team2;
-            this._scoreTeam2 = scoreTeam2;
+            this.team1 = team1;
+            this.scoreTeam1 = scoreTeam1;
+            this.team2 = team2;
+            this.scoreTeam2 = scoreTeam2;
         }
-
-
     }
 
     [TestClass]
@@ -56,50 +28,55 @@ namespace PrimitiveTypes
         public void TestTotalGoals()
         {
             FootballGame[] footballStage = {    new FootballGame("U Cluj", "CFR Cluj", 2, 3),
-                                                new FootballGame("Steaua Bucuresti", "Rapid Bucuresti", 3, 1),
-                                                new FootballGame("FC Brasov", "Astra Giurgiu", 4, 0),
+                                                new FootballGame("Steaua", "Rapid", 3, 1),
+                                                new FootballGame("FC Brasov", "Astra Giurgiu", 4, 1),
                                                 new FootballGame("FC Viitorul", "FC Botosani", 0, 5),
                                                 new FootballGame("Pandurii Targu Jiu", "Dinamo", 1, 4),
                                                 new FootballGame("FC Bacau", "ASA Targu Mures", 2, 2),
                                             };
             FootballGame[] lastGame = { new FootballGame("Concordia", "CSM Iasi", 4, 3) };
-            footballStage = AddLastGameToThisStage(footballStage, lastGame);
+            footballStage = AddLastGame(footballStage, lastGame);
             int totalGoals = CalculateTotalGoals(footballStage);
             float averageGoalsForThisStage = CalculateAverageGoalsForThisStage(footballStage);
-            Assert.AreEqual(34, totalGoals);
+            Assert.AreEqual(35, totalGoals);
         }
 
         [TestMethod]
-        public void TestRemoveTheGameWithHighestDifferenceOfGoals()
+        public void TestRemoveWeakestGame()
         {
             FootballGame[] footballStage = {    new FootballGame("U Cluj", "CFR Cluj", 2, 3),
-                                                new FootballGame("Steaua Bucuresti", "Rapid Bucuresti", 3, 1),
-                                                new FootballGame("FC Brasov", "Astra Giurgiu", 4, 0),
+                                                new FootballGame("Steaua", "Rapid", 3, 1),
+                                                new FootballGame("FC Brasov", "Astra Giurgiu", 4, 1),
                                                 new FootballGame("FC Viitorul", "FC Botosani", 0, 5),
                                                 new FootballGame("Pandurii Targu Jiu", "Dinamo", 1, 4),
                                                 new FootballGame("FC Bacau", "ASA Targu Mures", 2, 2),
                                             };
-            FootballGame[] lastGame = { new FootballGame("Concordia", "CSM Iasi", 4, 3) };
-            footballStage = AddLastGameToThisStage(footballStage, lastGame);
-            FootballGame[] footballStageWithoutTheGameWithHighestGoalDifference = RemoveGameWithHighestGoalDifference(footballStage);
+
+            FootballGame[] lastGame = { new FootballGame("Concordia", "CSM Iasi", 4, 30) };
+            FootballGame[] footballStageTest = AddLastGame(footballStage, lastGame);
+            FootballGame[] weekestFootballGameRemoved = RemoveWeakestGame(footballStageTest);
+            Assert.AreEqual(footballStage, weekestFootballGameRemoved);
+            
         }
 
         [TestMethod]
         public void TestLowestGoalRatio()
         {
             FootballGame[] footballStage = {    new FootballGame("U Cluj", "CFR Cluj", 2, 3),
-                                                new FootballGame("Steaua Bucuresti", "Rapid Bucuresti", 3, 1),
-                                                new FootballGame("FC Brasov", "Astra Giurgiu", 4, 0),
+                                                new FootballGame("Steaua", "Rapid", 3, 1),
+                                                new FootballGame("FC Brasov", "Astra Giurgiu", 4, 1),
                                                 new FootballGame("FC Viitorul", "FC Botosani", 0, 5),
                                                 new FootballGame("Pandurii Targu Jiu", "Dinamo", 1, 4),
                                                 new FootballGame("FC Bacau", "ASA Targu Mures", 2, 2),
                                             };
             FootballGame[] lastGame = { new FootballGame("Concordia", "CSM Iasi", 4, 3) };
-            footballStage = AddLastGameToThisStage(footballStage, lastGame);
+            footballStage = AddLastGame(footballStage, lastGame);
             string teamWithLowestGoalRatio = CalculateLowestGoalRatio(footballStage);
+            Assert.AreEqual("FC Viitorul", teamWithLowestGoalRatio);
         }
 
-        private FootballGame[] AddLastGameToThisStage(FootballGame[] footballStage, FootballGame[] lastGame)
+
+        private FootballGame[] AddLastGame(FootballGame[] footballStage, FootballGame[] lastGame)
         {
             Array.Resize(ref footballStage, footballStage.Length + 1);
             lastGame.CopyTo(footballStage, footballStage.Length - 1);
@@ -111,7 +88,7 @@ namespace PrimitiveTypes
             int totalGoals = 0;
             for (int i = 0; i < footballStage.Length; i++)
             {
-                totalGoals += footballStage[i].ScoreTeam1 + footballStage[i].ScoreTeam2;
+                totalGoals += footballStage[i].scoreTeam1 + footballStage[i].scoreTeam2;
             }
             return totalGoals;
         }
@@ -122,12 +99,14 @@ namespace PrimitiveTypes
             return totalGoals / footballStage.Length;
         }
 
-        private FootballGame[] RemoveGameWithHighestGoalDifference(FootballGame[] footballStage)
+        private FootballGame[] RemoveWeakestGame(FootballGame[] footballStage)
         {
             int gameWithHighestGoalDifference = CalculateGameWithHighestGoalDifference(footballStage);
-            Array.Clear(footballStage, gameWithHighestGoalDifference, 1);
+            int lenghtToCopy = footballStage.Length - gameWithHighestGoalDifference -1;
+            Array.ConstrainedCopy(footballStage, gameWithHighestGoalDifference + 1, footballStage, gameWithHighestGoalDifference, lenghtToCopy);
+            Array.Resize(ref footballStage, footballStage.Length - 1);
 
-            return footballStage;
+           return footballStage;
         }
 
         private int CalculateGameWithHighestGoalDifference(FootballGame[] footballStage)
@@ -136,8 +115,7 @@ namespace PrimitiveTypes
             int gameWithHighestGoalDifference = 0;
             for (int i = 0; i < footballStage.Length; i++)
             {
-                int gameGoalDifference = (footballStage[i].ScoreTeam1 >= footballStage[i].ScoreTeam2) ? footballStage[i].ScoreTeam1 - footballStage[i].ScoreTeam2
-                                                                                                      : footballStage[i].ScoreTeam2 - footballStage[i].ScoreTeam1;
+                int gameGoalDifference = GetGoalDifferenceInAGame(footballStage[i].scoreTeam1, footballStage[i].scoreTeam2);
                 if (gameGoalDifference > highestGoalDifference)
                 {
                     highestGoalDifference = gameGoalDifference;
@@ -147,38 +125,41 @@ namespace PrimitiveTypes
             return gameWithHighestGoalDifference;
         }
 
+        private int GetGoalDifferenceInAGame(int a, int b)
+        {
+            return (a >= b) ? a - b : b - a;
+        }
+
         private string CalculateLowestGoalRatio(FootballGame[] footballStage)
         {
             string teamWithLowestGameRatio = string.Empty;
-            int[] lowestRatio = new int[] {footballStage[0].ScoreTeam1, footballStage[0].ScoreTeam2};
-            teamWithLowestGameRatio = footballStage[0].Team1;
+            teamWithLowestGameRatio = footballStage[0].team1;
+            float lowestGameRatio = GetLowestRatio(footballStage[0].scoreTeam1, footballStage[0].scoreTeam2);
             for (int i = 0; i < footballStage.Length; i++)
             {
-                if (lowestRatio[0] > footballStage[i].ScoreTeam1)
+                float team1Ratio = GetLowestRatio(footballStage[i].scoreTeam1, footballStage[i].scoreTeam2);
+                float team2Ratio = GetLowestRatio(footballStage[i].scoreTeam2, footballStage[i].scoreTeam1);
+
+                if (team1Ratio <= team2Ratio && team1Ratio < lowestGameRatio)
                 {
-                    lowestRatio[0] = footballStage[i].ScoreTeam1;
-                    lowestRatio[1] = footballStage[i].ScoreTeam2;
-                    teamWithLowestGameRatio = footballStage[i].Team1;
-                } else if (lowestRatio[0] == footballStage[i].ScoreTeam1 && lowestRatio[1] < footballStage[i].ScoreTeam1)
+                    lowestGameRatio = team1Ratio;
+                    teamWithLowestGameRatio = footballStage[i].team1;
+                } else if (team2Ratio < lowestGameRatio)
                 {
-                    lowestRatio[1] = footballStage[i].ScoreTeam2;
-                    teamWithLowestGameRatio = footballStage[i].Team1;
+                    lowestGameRatio = team2Ratio;
+                    teamWithLowestGameRatio = footballStage[i].team2;
                 }
-                if (lowestRatio[0] > footballStage[i].ScoreTeam2)
-                {
-                    lowestRatio[0] = footballStage[i].ScoreTeam1;
-                    lowestRatio[1] = footballStage[i].ScoreTeam1;
-                    teamWithLowestGameRatio = footballStage[i].Team2;
-                }
-                else if (lowestRatio[0] == footballStage[i].ScoreTeam2 && lowestRatio[1] < footballStage[i].ScoreTeam2)
-                {
-                    lowestRatio[1] = footballStage[i].ScoreTeam1;
-                    teamWithLowestGameRatio = footballStage[i].Team2;
-                }
-                //Working here. NOT FINISHED.
+                
             }
 
             return teamWithLowestGameRatio;
+        }
+
+        private float GetLowestRatio(int scoreTeamA, int scoreTeamB)
+        {
+            float a = (float) scoreTeamA;
+            float b = (float) scoreTeamB;
+            return (b == 0) ? a : a / b;
         }
     }
 }
