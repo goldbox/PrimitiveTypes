d95b6425d1f38da3d2ce0238ce01f72724cd2bf0
timoteid <timoteidig@yahoo.com>
2015-10-23 09:32:19 +0300

    Presidential Election

--- SortPresidentialElection.cs
+++ SortPresidentialElection.cs
@@ -37,14 +37,23 @@ namespace PrimitiveTypes
                                                 new PollingStation("Mihai Eminescu", 497),
                                                 new PollingStation("Lucian Blaga", 346)
                                                 };
-            PollingStation[] finalResult = new PollingStation[4];
+            PollingStation[] test = {
+                                                new PollingStation("Mihai Eminescu", 1762),
+                                                new PollingStation("Lucian Blaga", 1653),
+                                                new PollingStation("Ion Creanga", 1401),
+                                                new PollingStation("Mircea Eliade", 1288),
+                                                };
+            PollingStation[] finalResult = new PollingStation[0];
             finalResult = AddThisPollingStation(clujGheorgheni, finalResult);
+            finalResult = AddThisPollingStation(clujManastur, finalResult);
+            finalResult = AddThisPollingStation(clujMarasti, finalResult);
+            CollectionAssert.AreEqual(test, finalResult);
         }
 
         public PollingStation[] AddThisPollingStation(PollingStation[] newArrivedPolling, PollingStation[] finalResult)
         {
             finalResult = AddNewVotesToFinal(newArrivedPolling, finalResult);
-            //finalResult = SortUsingNumberOfVotes(finalResult);
+            finalResult = SortUsingNumberOfVotes(finalResult);
             return finalResult;
         }
 
@@ -57,8 +66,37 @@ namespace PrimitiveTypes
 
         public void AddVotesForThisCandidate(PollingStation candidate, ref PollingStation[] finalResult)
         {
-
+            bool nameMissing = true;
+            var lastIndex = finalResult.Length;
+            for (int i = 0; i < lastIndex; i++)
+                if (string.Equals(finalResult[i].name, candidate.name))
+                {
+                    nameMissing = false;
+                    finalResult[i].numberOfVotes += candidate.numberOfVotes;
+                }
+            if (nameMissing)
+            {
+                Array.Resize(ref finalResult, lastIndex + 1);
+                finalResult[lastIndex].name = candidate.name;
+                finalResult[lastIndex].numberOfVotes = candidate.numberOfVotes; 
+            }
         }
+
+        public PollingStation[] SortUsingNumberOfVotes(PollingStation[] finalResult)
+        {
+            for (int i = 1; i < finalResult.Length; i++)
+                for (int j = i; j > 0 && (finalResult[j].numberOfVotes > finalResult[j - 1].numberOfVotes); j--)
+                    Swap(ref finalResult, j);
+            return finalResult;
+        }
+
+        private void Swap(ref PollingStation[] finalResult, int j)
+        {
+            var temp = finalResult[j];
+            finalResult[j] = finalResult[j - 1];
+            finalResult[j - 1] = temp;
+        }
+
 
     }
 }
